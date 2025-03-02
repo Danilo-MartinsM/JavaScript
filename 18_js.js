51a5eb086f22827cddba892c1a230702f71b309f
Danilo-MartinsM <danilo.martins.dev@gmail.com>
2025-03-02 20:53:04 -0300

    Create 18_js.js

---
+++ 18_js.js
@@ -0,0 +1,11 @@
+let numeros = [];
+let soma = 0;
+
+for (let i = 0; i < 5; i++){
+    let numero = Number(prompt('Digite o numero '+ (i + 1) +':'));
+    numeros.push(numero)
+}
+
+for (let i = 0; i < numeros.length; i++){
+    soma += numeros[i];
+}
