8f3405e99f5740f9043f60bf590f53c8828c4cc8
Danilo-MartinsM <danilo.martins.dev@gmail.com>
2025-03-02 19:32:02 -0300

    Create 17_js.js

---
+++ 17_js.js
@@ -0,0 +1,20 @@
+let palavras = [];
+
+for (let i = 0; i < 5; i ++){
+    let palavra = prompt('Digite a palavra '+(i + 1) +':');
+    palavras.push(palavra);
+}
+let frequencia = {};
+
+for (let i = 0; i < palavras.length; i ++){
+    let palavra = palavras[i];
+
+    if (frequencia[palavra]) {
+        frequencia[palavra]++;
+    }
+    else {
+        frequencia[palavra] = 1;
+    }
+}
+
+console.log(frequencia);
