0347b02180e5fcd668d91be8f49f7d47383c2f43
Danilo-MartinsM <danilo.martins.dev@gmail.com>
2025-03-09 22:05:17 -0300

    Update 18_js.js

--- 18_js.js
+++ 18_js.js
@@ -1,6 +1,8 @@
 let numeros = [];
 let soma = 0;
 
+const prompt = require('prompt-sync')();
+
 for (let i = 0; i < 5; i++){
     let numero = Number(prompt('Digite o numero '+ (i + 1) +':'));
     numeros.push(numero)
